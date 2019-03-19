bf18e9932220aa80ffc26c36d4de53cec1050092
Michael Denholtz <31019257+denholtz@users.noreply.github.com>
2019-03-19 14:08:15 -0700

    Changed 'key' column in API Key table to 'uuid'

--- db/build/0065_tblApi_Keys.sql
+++ db/build/0065_tblApi_Keys.sql
@@ -8,7 +8,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[tblApi_Keys](
-	[Key] [char] (36) PRIMARY KEY,
+	[uuid] [char] (36) PRIMARY KEY,
 	[UserID] [int] NOT NULL,
 	CONSTRAINT [FK_UserID] FOREIGN KEY (UserID)
 	REFERENCES [dbo].[tblUsers] (UserID)
