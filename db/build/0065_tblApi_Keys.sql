23940497c6b6e60aa0877d92f483defe95a8c99f
Michael Denholtz <31019257+denholtz@users.noreply.github.com>
2019-03-22 16:06:57 -0700

    Added FK index and primary FG placement to tblApi_Keys build script

--- db/build/0065_tblApi_Keys.sql
+++ db/build/0065_tblApi_Keys.sql
@@ -12,5 +12,22 @@ CREATE TABLE [dbo].[tblApi_Keys](
 	[UserID] [int] NOT NULL,
 	CONSTRAINT [FK_UserID] FOREIGN KEY (UserID)
 	REFERENCES [dbo].[tblUsers] (UserID)
-)
-	
+) ON [PRIMARY]
+GO
+		
+		    
+---------------------
+-- Indices
+---------------------
+		    
+USE [Opedia]
+GO
+
+
+
+CREATE NONCLUSTERED INDEX [IX_tblApi_Keys_UserID] ON [dbo].[tblApi_Keys]
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
+ON [PRIMARY]
+GO
