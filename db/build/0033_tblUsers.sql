841dcbb96ac2200869c1f422f5c1e3085f17d668
Michael Denholtz <31019257+denholtz@users.noreply.github.com>
2019-07-24 14:39:38 -0700

    Updated user table build script

--- db/build/0033_tblUsers.sql
+++ db/build/0033_tblUsers.sql
@@ -48,4 +48,7 @@ CREATE UNIQUE NONCLUSTERED INDEX [idxUsername] ON [dbo].[tblUsers]
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 
+INSERT INTO [dbo].[tblUsers] (FirstName, FamilyName, Username, Password, Email)
+VALUES ('Unregistered','User','UnregisteredUser','noauth','unregistereduser@simonscmap.com')
+
 
